ea28a30c4076cd19eab10621b78672fba20fc389
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-06-03 23:20:09 +0530

    new

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Contact from './pages/Contact'
 import About from './pages/About'
-import Navbar from './components/Navigation/Navbar'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import Video from '../components/Home/Video'
+import Video from '../components/Video'
 
 const Home = () => {
   return (
     <div>
       <div className='h-screen w-screen fixed'>
         <Video />
-      </div>
-      <div className='h-screen w-screen relative pb-5 overflow-hidden flex flex-col justify-between'>
       </div>
     </div>
   )
