2ddc33201f6e03f9c49ed12609962dfbcf447bb8
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-06-03 23:24:03 +0530

    new

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,20 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import Contact from './pages/Contact'
-import About from './pages/About'
-import Navbar from './components/Navbar'
+import React from "react";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/projects' element={<Projects />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/about' element={<About />} />
-      </Routes>
+      <Home />
+      <Projects />
+      <Contact />
+      <About />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
