5668902bcfaccf4fef770a4a518d76f8a9b004e2
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-08-08 23:43:47 +0530

    new

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <div className='h-screen w-screen fixed text-amber-700'>
+      <div className="h-screen w-screen fixed text-amber-700">
         <h1>hii</h1>
         <h2>hello git</h2>
         <h1>here we go again</h1>
+        <h1>same as before but changing</h1>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
